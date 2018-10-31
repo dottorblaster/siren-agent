5f15195ed6112ad92933b318bfbb3b70cae8ca9b
Alessio Biancalana <dottorblaster@gmail.com>
2018-10-31 15:44:44 +0100

    feat: first commit, full functionality

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate actix_web;
+
+use std::env;
+use actix_web::{server, App, HttpRequest, Responder, Json};
+
+#[derive(Serialize)]
+struct Response {
+    hostname: String,
+    status: String,
+}
+
+fn get_from_env(name: &str) -> String {
+    match env::var(name) {
+        Ok(value) => value,
+        Err(_) => String::from("unset"),
+    }
+}
+
+fn index(_req: &HttpRequest) -> impl Responder {
+    let host_name = get_from_env("HOSTNAME");
+    Json(Response {
+        hostname: host_name,
+        status: String::from("OK"),
+    })
+}
+
+fn main() {
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("127.0.0.1:9999")
+        .unwrap()
+        .run();
+}
